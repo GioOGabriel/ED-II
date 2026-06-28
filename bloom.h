505b52655f2d00b4d91cc442614685a740718507
Giovani Gabriel <giovanigabriel@outlook.com>
2026-06-28 18:01:20 -0300

    Adicionado 3 funçãozinha pra facilitar minha vida

--- bloom.h
+++ bloom.h
@@ -16,5 +16,8 @@ FiltroBloom* bloom_criar(int tamanho_bits, int total_funcoes_hash);
 void bloom_inserir(FiltroBloom *fb, const char *nome_usuario);
 bool bloom_buscar(FiltroBloom *fb, const char *nome_usuario);
 void bloom_destruir(FiltroBloom *fb);
+void bloom_recomendar_dimensoes(int num_elementos, double taxa_falso_positivo, int *tamanho_bits, int *num_funcoes_hash);
+double bloom_calcular_falso_positivo(int tamanho_bits, int num_funcoes_hash, int num_elementos);
+double bloom_taxa_ocupacao(FiltroBloom *fb);
 
 #endif
